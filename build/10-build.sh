7eeb8970316c50f03138eb1f08b80b2fe7ccb0d3
Manfred Lotz <manfred.lotz@posteo.de>
2026-05-28 20:18:41 +0200

    first customization

--- build/10-build.sh
+++ build/10-build.sh
@@ -33,7 +33,7 @@ mkdir -p /usr/share/ublue-os/homebrew/
 cp /ctx/custom/brew/*.Brewfile /usr/share/ublue-os/homebrew/
 
 # Consolidate Just Files
-find /ctx/custom/ujust -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >> /usr/share/ublue-os/just/60-custom.just
+find /ctx/custom/ujust -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >>/usr/share/ublue-os/just/60-custom.just
 
 # Copy Flatpak preinstall files
 mkdir -p /etc/flatpak/preinstall.d/
@@ -44,7 +44,16 @@ echo "::endgroup::"
 echo "::group:: Install Packages"
 
 # Install packages using dnf5
-# Example: dnf5 install -y tmux
+
+# install additional packages
+dnf5 install -y snapper
+mkdir -p -m 755 /usr/libexec/snapper/plugins
+
+dnf5 install -y dovecot dovecot-pigeonhole
+dnf5 install -y opensmtpd
+
+# getting more SELinux related  man pages and documentation for SELinux policy modules
+dnf5 install -y selinux-policy-doc
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
